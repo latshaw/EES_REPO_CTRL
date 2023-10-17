772c52defe88b017541a12bd92fb8e6ff75fd374
James Latshaw <110061660+latshaw@users.noreply.github.com>
2023-10-17 14:45:25 -0400

    Update make.sh to fix .sof name

--- LLRF3_RES/make.sh
+++ LLRF3_RES/make.sh
@@ -12,6 +12,6 @@ echo 'GENERATING PROJECT '$PROJECT' FROM GIT REPO COMMIT ID '$GIT_ID_FULL'...'
 quartus_sh -t $TCL
 quartus_sh --flow compile $PROJECT
 cd output_files
-quartus_cpf -o auto_create_rpd=on -c -d MT25QU01G -s 10CX105Y $PROJECT'_'$GIT_ID.sof $PROJECT'_'$GIT_ID.jic
+quartus_cpf -o auto_create_rpd=on -c -d MT25QU01G -s 10CX105Y $PROJECT.sof $PROJECT'_'$GIT_ID.jic
 
 echo 'DONE!'
