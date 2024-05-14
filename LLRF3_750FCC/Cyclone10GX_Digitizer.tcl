47490f8c76ff1a9cb4842e778661dab9a5b778c0
James Latshaw <latshaw@jlab.org>
2024-05-14 14:31:47 -0400

    updaging pin assignment for tcl

--- LLRF3_750FCC/Cyclone10GX_Digitizer.tcl
+++ LLRF3_750FCC/Cyclone10GX_Digitizer.tcl
@@ -409,7 +409,7 @@ if {$make_assignments} {
 	set_location_assignment PIN_A4 -to fpga_ver[5]
 	set_location_assignment PIN_A12 -to fpga_ver[3]
 	set_location_assignment PIN_E16 -to lmk04808_ld
-	set_location_assignment PIN_Y9 -to jtag_mux_sel_out
+	set_location_assignment PIN_T2 -to jtag_mux_sel_out
 	# Commit assignments
 	export_assignments
 
