82b3fb830b81626eadc303b80a3676d09ea8502a
James Latshaw <latshaw@jlab.org>
2024-06-05 15:02:36 -0400

    r'

--- C10GXFACTORY/fpga_tsd_int/fpga_tsd_int_bb.v
+++
@@ -1,8 +0,0 @@
-module fpga_tsd_int (
-		input  wire       corectl, // corectl.corectl
-		input  wire       reset,   //   reset.reset
-		output wire [9:0] tempout, // tempout.tempout
-		output wire       eoc      //     eoc.eoc
-	);
-endmodule
-
--- C10GXFACTORY/xcvr_phy_pll/xcvr_phy_pll_bb.v
+++
@@ -1,9 +0,0 @@
-module xcvr_phy_pll (
-		input  wire  pll_powerdown, // pll_powerdown.pll_powerdown
-		input  wire  pll_refclk0,   //   pll_refclk0.clk
-		output wire  tx_serial_clk, // tx_serial_clk.clk
-		output wire  pll_locked,    //    pll_locked.pll_locked
-		output wire  pll_cal_busy   //  pll_cal_busy.pll_cal_busy
-	);
-endmodule
-
