770daf37245de2e2cf3ca262a1565b9a9e5c63e0
James Latshaw <latshaw@jlab.org>
2024-09-25 14:27:40 -0400

    Rama moved cominational logic outside of the process statment in jtag_sel.vhd

--- llrf3p0_power_sequence/jtag_sel.vhd
+++ llrf3p0_power_sequence/jtag_sel.vhd
@@ -13,9 +13,10 @@ end entity jtag_sel;
 architecture behavior of jtag_sel is
 
 signal jtag_out_q, jtag_out_d		:	std_logic;
-signal jtag_sel_in_q					:	std_logic_vector(5 downto 0);
+signal jtag_sel_in_q					:	std_logic_vector(2 downto 0);
 
-
+signal c10_cnt_d, c10_cnt_q		:	integer range 0 to 7;
+signal m10_cnt_d, m10_cnt_q		:	integer range 0 to 7;
 
 
 
@@ -31,26 +32,24 @@ jtag_out_d		<=	--'1'when jtag_sel_in_q(2 downto 1) = "10" else
 						
 						
 						
-
+--m10_cnt_d		<=	3 when m10_cnt_q = 3 else m10_cnt_q + 1;						
+--c10_cnt_d		<=	3 when c10_cnt_q = 3 else c10_cnt_q + 1;
 						
 						
---jtag_sel_out	<=	not jtag_out_q;						
+jtag_sel_out	<=	not jtag_out_q;						
 
 
-process(clock)
+process(reset, clock)
 begin
-	if(rising_edge(clock)) then
-	
-		if(reset = '0') then
+	if(reset = '0') then
 			jtag_out_q	<=	'1';
 			jtag_sel_in_q	<=	(others	=>	'0');
-			jtag_sel_out	<=	'0';			
-		else
-			jtag_out_q						<=	jtag_out_d;
+	elsif(rising_edge(clock) then		
+			jtag_out_q	<=	jtag_out_d;
 			jtag_sel_in_q(2 downto 0)	<=	jtag_sel_in_q(1 downto 0)&jtag_sel_in(0);
-			jtag_sel_in_q(5 downto 3)	<=	jtag_sel_in_q(4 downto 3)&jtag_sel_in(1);
-			jtag_sel_out					<=	not jtag_out_q;
-		end if;
+			jtag_sel_in_q(5 downto 3)	<=	jtag_sel_in_q(5 downto 4)&jtag_sel_in(1);
+			
+
 	end if;	
 end process;
 	
