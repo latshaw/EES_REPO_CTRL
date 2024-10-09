251e5b6472b1785204e148f4bbaefd1fc9b91e01
James Latshaw <latshaw@jlab.org>
2024-10-09 14:07:25 -0400

    jtag sel initial pulse counter must be >> reset all counter. the c10 must be online when this happens

--- llrf3p0_power_sequence/reset_all.vhd
+++ llrf3p0_power_sequence/reset_all.vhd
@@ -12,16 +12,16 @@ end entity reset_init;
 
 architecture behavior of reset_init is
 
-signal rst_cnt_d, rst_cnt_q	:	unsigned(11 downto 0);
+signal rst_cnt_d, rst_cnt_q	:	unsigned(15 downto 0);
 signal rst_out_d, rst_out_q	:	std_logic;
 
 begin
 
 
-rst_cnt_d	<=	rst_cnt_q + 1 when rst_cnt_q /= x"FFF" else
+rst_cnt_d	<=	rst_cnt_q + 1 when rst_cnt_q /= x"FFFF" else
 					rst_cnt_q;
 					
-rst_out_d	<=	'0' when rst_cnt_q /= x"FFF" else '1';
+rst_out_d	<=	'0' when rst_cnt_q /= x"FFFF" else '1';
 
 reset_out	<=	rst_out_q;
 
