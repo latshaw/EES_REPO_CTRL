e2d0065f2472cda33c21067023f6e845fd41a022
AESGEESAMAN <geesaman@jlab.org>
2026-09-15 10:52:00 -0400

    New version of ADC block added, cleaned up formatting and added range select.

---
+++ LLRF3_HTR/ADS8688v2.vhd
@@ -0,0 +1,255 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE WORK.COMPONENTS.ALL;
+
+
+entity ADS8688v2 is
+	port(	clock					: in std_logic;
+			reset					: in std_logic;
+			filter_control		: in std_logic_vector(15 downto 0);
+			SDO					: in std_logic;
+			
+			SDI					: out std_logic;
+			CS						: out std_logic;
+			SCLK					: out std_logic;
+			DATA_OUT 			: OUT REG16_ARRAY
+			);
+			
+end entity ADS8688v2;
+
+architecture behavior of ADS8688v2 is
+
+	COMPONENT FILTERS IS
+		PORT(	CLOCK 				: IN STD_LOGIC;
+				STROBE				: IN STD_LOGIC;
+				RESET 				: IN STD_LOGIC;
+				FILTER_CONTROL		: IN STD_LOGIC_VECTOR(15 downto 0);
+				DATA_IN 				: IN STD_LOGIC_VECTOR(15 downto 0);
+				DATA_OUT 			: OUT STD_LOGIC_VECTOR(15 downto 0)
+				);
+	END COMPONENT;
+
+--	component regne is
+--		generic map(n => 16)
+--		port( clock			: in std_logic;
+--				reset			: in std_logic;
+--				clear			: in std_logic;
+--				en				: in std_logic;
+--				input			: in std_logic_vector(15 downto 0);
+--				output		: out std_logic_vector(15 downto 0)
+--				);
+--	end component;
+
+	type	 state_type is (init, cs_low, cs_low_wait, load_cntrl_reg, sclk_low_cntrl, sclk_high_cntrl, cs_high, wait_cs_high, data_cs_low, data_cs_low_wait, sclk_data_high, sclk_data_low, data_cs_high, data_cs_high_wait, data_acquired);
+
+	type	RegisterRecord is record
+	sdo					: std_logic;
+	sclk					: std_logic;
+	cs						: std_logic;
+	sdi					: std_logic;
+	state 				: state_type;
+	SPI_data				: std_logic_vector(31 downto 0);
+	bit_count			: integer range 0 to 31;
+	input_din_reg		: std_logic_vector(15 downto 0);
+	sclkDivider			: integer range 0 to 7;
+	cntrl_count			: integer range 0 to 8;
+	ActiveChannel		: integer range 0 to 7;
+	ADC_data				: REG16_ARRAY;
+	end record RegisterRecord;
+	
+	signal d, q					: RegisterRecord;  
+	signal channel_enable	: std_logic_vector(7 downto 0);
+	signal RangeSelect		: std_logic_vector(7 downto 0);
+	
+	signal InputShiftReg		: std_logic_vector(15 downto 0);
+
+	
+	begin
+	
+		sdi				<= q.sdi;
+		cs					<= q.cs;
+		sclk				<= q.sclk;
+		d.sdo				<= sdo;
+			
+		--Filter selection for ADC data
+		ADCDataFilter : for i in 0 to 7 generate
+			ADC_Filter: FILTERS
+				port map(	clock 			=>	clock,
+								strobe			=>	channel_enable(i),
+								reset				=>	reset,
+								filter_control	=> filter_control,
+								data_in			=>	q.ADC_data(i),
+								data_out			=> DATA_OUT(i)
+								);
+			end generate;	
+					
+			
+		-- 0x6 = 0-5.12V must be written for each channel
+		RangeSelect	<= x"06";
+
+		--Set command word for output
+		InputShiftReg	<= x"05" & RangeSelect when d.cntrl_count = 0 else --Range select ch0
+								x"06" & RangeSelect when d.cntrl_count = 1 else --Range select ch1
+								x"07" & RangeSelect when d.cntrl_count = 2 else --Range select ch2
+								x"08" & RangeSelect when d.cntrl_count = 3 else --Range select ch3
+								x"09" & RangeSelect when d.cntrl_count = 4 else --Range select ch4
+								x"0A" & RangeSelect when d.cntrl_count = 5 else --Range select ch5
+								x"0B" & RangeSelect when d.cntrl_count = 6 else --Range select ch6
+								x"0C" & RangeSelect when d.cntrl_count = 7 else --Range select ch7
+								--Auto channel incrementing.
+								x"A000" when d.cntrl_count = 8 else
+								(others => '0');
+		
+		process(clock, reset)
+		begin
+			if(reset = '0') then
+				q.sdo					<=	'0';
+				q.sdi					<=	'0';
+				q.cs					<=	'1';
+				q.sclk				<= q.sclk;
+				q.state				<= init;
+				q.SPI_data			<= (others => '0');
+				q.bit_count			<= 0;
+				q.input_din_reg	<= (others => '0');
+				q.sclkDivider		<= 0;
+				q.cntrl_count		<= 0;
+				q.SPI_data			<= (others => '0');
+				q.ActiveChannel	<= 0;
+				for i in 0 to 7 loop
+					q.ADC_data(i)	<= (others => '0');
+				end loop;
+			elsif(rising_edge(clock)) then
+				q				<= d;
+			end if;
+		end process;
+		
+		process(clock, reset)
+		begin
+			if(reset = '0') then
+				d.state <= init;
+			elsif(clock = '1' and clock'event) then
+				case q.state is
+					--Start
+					when init 						=> 
+						d.sdi 						<= '0';
+						d.sclk						<= '1';
+						d.cs							<= '1';
+						d.state						<= cs_low;
+					--Programming ADC registers
+					when cs_low						=> 
+						d.cs							<= '0';
+						d.state						<= cs_low_wait;
+					when cs_low_wait				=>
+						d.state <= load_cntrl_reg;
+					when load_cntrl_reg			=> 
+						d.sdi							<= q.input_din_reg(15);
+						d.input_din_reg			<= InputShiftReg;
+						d.state						<= sclk_high_cntrl;
+					when sclk_high_cntrl 		=>
+						d.sclk						<= '1';
+						d.sdi							<= q.input_din_reg(15);
+						d.sclkDivider				<= q.sclkDivider + 1;
+						if(d.sclkDivider = 3) then 
+							d.state					<= sclk_low_cntrl;
+						end if;
+					when sclk_low_cntrl 			=>
+						d.sclk						<= '0';
+						d.sdi							<= q.input_din_reg(15);
+						if(d.sclkDivider = 7) then
+							--Shift next bit into the output.
+							d.input_din_reg		<= q.input_din_reg(14 downto 0) & '0';
+							d.sclkDivider 			<= 0;
+							if(q.bit_count = 23) then
+								d.bit_count 		<= 0;
+								d.state 				<= cs_high;
+							else
+								d.bit_count 		<= q.bit_count + 1;
+								d.state 				<= sclk_high_cntrl;
+							end if;
+						else
+							d.state <= sclk_low_cntrl;
+						end if;
+					--End of register programming
+					when cs_high 					=>
+						d.cs							<= '1';
+						d.sclk						<= '1';
+						d.sdi							<= q.input_din_reg(15);
+						d.state						<= wait_cs_high;
+					when wait_cs_high				=> 
+						d.sdi							<= q.input_din_reg(15);
+						if(d.cntrl_count = 8) then
+							d.state					<= data_cs_low;
+							d.cntrl_count			<= 0;
+						else
+							d.state					<= cs_low;
+							d.cntrl_count			<= q.cntrl_count + 1;
+						end if;
+					when data_cs_low 				=>
+						d.cs							<= '0';
+						d.sdi							<= '0';
+						d.state						<= data_cs_low_wait;
+					when data_cs_low_wait		=>
+						d.state						<= sclk_data_high;
+					when sclk_data_high 			=>
+						d.sclkDivider				<= q.sclkDivider + 1;
+						d.sclk						<= '1';
+						if(q.sclkDivider = 3) then 
+							d.SPI_data				<= q.SPI_data(30 downto 0) & q.sdo;
+							d.state 					<= sclk_data_low;
+						end if;
+					when sclk_data_low 			=>
+						d.sclk						<= '0';
+						if(q.sclkDivider = 7) then
+							d.sclkDivider			<= 0;
+							if(d.bit_count = 31) then 
+								q.bit_count			<= 0;
+								d.state 				<= data_cs_high;
+							else
+								d.bit_count			<= q.bit_count + 1;
+								d.state <= sclk_data_high;
+							end if;
+						else
+							d.sclkDivider			<= q.sclkDivider + 1;
+						end if;
+					when data_cs_high				=>
+						d.cs							<= '1';
+						d.sclk						<= '1';
+						d.state 						<= data_cs_high_wait;
+					when data_cs_high_wait		=>
+						d.state						<= data_acquired;
+					when data_acquired			=>
+						if(q.ActiveChannel = 0) then
+							d.ADC_data(0)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 1;
+						elsif(q.ActiveChannel = 1) then
+							d.ADC_data(1)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 2;
+						elsif(q.ActiveChannel = 2) then
+							d.ADC_data(2)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 3;
+						elsif(q.ActiveChannel = 3) then
+							d.ADC_data(3)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 4;
+						elsif(q.ActiveChannel = 4) then
+							d.ADC_data(4)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 5;
+						elsif(q.ActiveChannel = 5) then
+							d.ADC_data(5)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 6;
+						elsif(q.ActiveChannel = 6) then
+							d.ADC_data(6)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 7;
+						elsif(q.ActiveChannel = 7) then
+							d.ADC_data(7)			<= q.SPI_data(15 downto 0);
+							d.ActiveChannel		<= 0;
+						end if;
+						d.state						<= data_cs_low;
+						
+					when others 					=> 
+						d.state <= init;
+				end case;
+			end if;
+		end process;
+		
+		
+end architecture behavior;
